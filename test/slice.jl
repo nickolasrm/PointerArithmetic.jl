40e43b044c67968533ec5755f5d8a8e3460adc7e
nickolasrm <nickolasrochamachado@gmail.com>
2021-04-18 09:08:46 -0300

    More tests and coveralls
    - Added more tests for slice of slice, size and interval functions
    
    - Coveralls tag added to travis.yml

--- test/slice.jl
+++ test/slice.jl
@@ -3,7 +3,7 @@
     b = Slice(a, 2:4)
     @test b[1] == a[2]
     @test b[0] == a[1]
-    b = b << -1
+    b = b << -1    
     @test length(b) == 4
     @test b[1] == a[1]
     b = b >> -1
@@ -21,4 +21,25 @@
     @test_broken b << 2
     @test_broken b >> 2
     @test_broken b >> -2
+
+    @test_broken b[4]
+    @test_broken b[-1]
+    @test_broken b[4] = 2
+    @test_broken b[-1] = 2
+    
+    @test size(b) == (2,)
+    @test interval(b) == 2:3
+    
+    c = Slice(b, 1:3)
+    @test length(c) == 3
+    @test c[3] == 10000
+    c = Slice(b, 1:1)
+    @test length(c) == 1
+    @test b[1] == 2
+    c = Slice(b, 0:3)
+    @test sum(c .== a) == length(c)
+    @test_broken Slice(b, -1:3)
+    @test_broken Slice(b, 0:4)
+    @test_broken Slice(b, -1:4)
+
 end
